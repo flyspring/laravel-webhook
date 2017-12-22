ec33cafc55ed5b5af19ae1dd1813e05f4fd2c02e
abel16 <wenxuefei@daiba.com>
2017-12-22 12:09:00 +0800

    修改服务

--- src/WebhookServiceProvider.php
+++ src/WebhookServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace FlySpring\LaravelWebHook;
 
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 class WebhookServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,25 @@ class WebhookServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->setupConfig();
+    }
+    
+    /**
+     * Setup the config.
+     *
+     * @return void
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__.'/../config/hooks.php');
+    
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('hooks.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('hooks');
+        }
+    
+        $this->mergeConfigFrom($source, 'hooks');
     }
 
     /**
